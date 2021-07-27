da87564c82c5bd4e2c4cb29765996f8b30757de5
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2021-07-27 13:52:37 +0530

    Create Template_May2021_[Beginners].cpp

---
+++ Template_May2021_[Beginners].cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define int long long
+#define mod 1000000007
+using namespace std;
+
+void solve(){
+
+
+}
+
+int32_t main()
+{
+  int test;
+  cin>>test;
+  while(test>0){
+  
+    solve();
+    test--;
+    
+  }
+
+  return 0;
+}
