3c616177da25d370b12d8ec7969b291bafe61514
Kamlesh Bisht <75537446+kamlesh012@users.noreply.github.com>
2021-10-01 17:26:39 +0530

    Extra Macros Eliminated.
    Updated on 1 October 2021 [ 1066 CF & 1604 CC ]

--- TemplateOctober2021.cpp
+++ TemplateOctober2021.cpp
@@ -1,40 +1,39 @@
-//Competitive Programming -Starter Template
 //<---------------------Kamlesh Singh Bisht--------------------->
 #include <bits/stdc++.h>
-#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define cstrt clock_t start, end; double ctd; start = clock();
-#define cend end = clock(); ctd = ((double) (end - start)) / CLOCKS_PER_SEC;cout<<ctd<<"sec"<<endl;
 #define int long long
 #define mod 1000000007
 #define pi pair<int,int>
 #define vi vector<int>
 #define vii vector<pi>
 #define pb push_back
-#define a first
-#define b second
+#define f first
+#define s second
 #define nl '\n'
 #define rep(i,n,s) for(int i=0;i<n;i+=s)
 #define fr(i,n,s) for(int i=1;i<=n;i+=s)
 #define dbg(x) cout<<#x<<"->"<<x<<nl;
 #define ebg(x,y) cout<<#x<<"->"<<x<<" "<<#y<<"->"<<y<<nl;
-
 using namespace std;
 
+<---------------------------------Road To Pupil-------------------------------->
+	
 void solve()
 {
+
+	
 
 }
 
 int32_t main()
 {
-	// cstrt
-	fastio;
-	int t;
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	
+	int t=1;
 	cin >> t;
 	while (t--)
 	{
 		solve();
-	}
-	// cend
+	}	
 	return 0;
 }
